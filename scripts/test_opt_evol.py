6a9ac62cdd9427549951bd048eaa8f02530d2fd9
Hung V. Tran <h.v.tran@uq.edu.au>
2023-08-22 12:45:20 +1000

    WIP Add PoC for OptEmbed with LightGCN

---
+++ scripts/test_opt_evol.py
@@ -0,0 +1,102 @@
+import argparse
+import os
+from typing import Dict, Optional, Sequence
+
+import torch
+import yaml
+from torch.utils.data import DataLoader
+
+from src.dataset.cf_graph_dataset import CFGraphDataset, TestCFGraphDataset
+from src.loggers import Logger
+from src.models import get_graph_model
+from src.models.opt_embed import evol_search_lightgcn
+
+
+def get_config(argv: Optional[Sequence[str]] = None) -> Dict:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config_file")
+    args = parser.parse_args(argv)
+    with open(args.config_file) as fin:
+        config = yaml.safe_load(fin)
+    return config
+
+
+def main(argv: Optional[Sequence[str]] = None):
+    config = get_config(argv)
+    logger = Logger(**config["logger"])
+
+    # Loading train dataset
+    logger.info("Load train dataset...")
+    train_dataloader_config = config["train_dataloader"]
+    train_dataset_config = train_dataloader_config["dataset"]
+    train_dataset = CFGraphDataset(**train_dataset_config)
+    logger.info("Successfully load train dataset")
+    train_dataset.describe()
+
+    if config["run_test"]:
+        val_dataloader_config = config["test_dataloader"]
+    else:
+        val_dataloader_config = config["val_dataloader"]
+
+    logger.info("Load val dataset...")
+    val_dataset = TestCFGraphDataset(val_dataloader_config["dataset"]["path"])
+    val_dataloader = DataLoader(
+        val_dataset,
+        val_dataloader_config["batch_size"],
+        shuffle=False,
+        collate_fn=TestCFGraphDataset.collate_fn,
+        num_workers=val_dataloader_config["num_workers"],
+    )
+    logger.info("Successfully load val dataset")
+
+    checkpoint_folder = os.path.dirname(config["checkpoint_path"])
+    os.makedirs(checkpoint_folder, exist_ok=True)
+
+    model_config = config["model"]
+    model = get_graph_model(
+        train_dataset.num_users,
+        train_dataset.num_items,
+        model_config,
+    )
+
+    if torch.cuda.is_available():
+        pass
+    else:
+        pass
+
+    if config["run_test"]:
+        raise NotImplementedError("Will implement later")
+
+    num_params = 0
+    for p in model.parameters():
+        num_params += p.numel()
+    logger.log_metric("num_params", num_params)
+
+    logger.info(f"Model config: {model_config}")
+
+    checkpoint = torch.load(config["checkpoint_path"])
+    model.load_state_dict(checkpoint["state_dict"])
+
+    # TODO: Refactor later
+    num_generations = 5
+    population = 6
+    n_crossover = 4
+    n_mutate = 4
+    p_mutate = 0.2
+    k = 5
+    result = evol_search_lightgcn(
+        model,
+        num_generations,
+        population,
+        n_crossover,
+        n_mutate,
+        p_mutate,
+        k,
+        val_dataloader,
+        train_dataset,
+    )
+    return result
+
+
+if __name__ == "__main__":
+    result = main()
--- src/models/embeddings.py
+++ src/models/embeddings.py
@@ -293,6 +293,10 @@ def get_embedding(
             hidden_size,
             **embedding_config,
         )
+    elif name == "optembed":
+        from src.models.opt_embed import OptEmbed
+
+        emb = OptEmbed(num_item, hidden_size)
     elif name not in name_to_cls:
         raise NotImplementedError()
     else:
