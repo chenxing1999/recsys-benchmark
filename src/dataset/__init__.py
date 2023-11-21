93c1353d3aa97fc62850b4beeb5a4764ef2e3d99
Hung V. Tran <h.v.tran@uq.edu.au>
2023-11-21 15:48:54 +1000

    Add get ctr dataset

--- src/dataset/__init__.py
+++ src/dataset/__init__.py
@@ -1,9 +1,14 @@
 from typing import Any, Dict, Optional
 
 from src.dataset.base import ICTRDataset
+from src.dataset.criteo import get_dataset_cls
 
 
 def get_ctr_dataset(
     dataloader_config: Dict[str, Any], train_info_to_val: Optional[Dict] = None
 ) -> ICTRDataset:
-    raise NotImplementedError()
+    if train_info_to_val is None:
+        train_info_to_val = {}
+
+    dataset_cls = get_dataset_cls(dataloader_config)
+    return dataset_cls(**dataloader_config, **train_info_to_val)
