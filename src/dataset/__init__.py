c81e50df2d7e7de1323f704a6680ec72ba86cc8e
Hung V. Tran <h.v.tran@uq.edu.au>
2023-11-22 13:10:15 +1000

    Add code for Avazu on RAM

--- src/dataset/__init__.py
+++ src/dataset/__init__.py
@@ -1,5 +1,8 @@
 from typing import Any, Dict, Optional
 
+from loguru import logger
+
+from src.dataset.avazu.avazu_on_ram import AvazuOnRam
 from src.dataset.base import ICTRDataset
 from src.dataset.criteo import get_dataset_cls
 
@@ -10,5 +13,15 @@ def get_ctr_dataset(
     if train_info_to_val is None:
         train_info_to_val = {}
 
-    dataset_cls = get_dataset_cls(dataloader_config)
-    return dataset_cls(**dataloader_config, **train_info_to_val)
+    dataset_config: Dict = dataloader_config["dataset"]
+    name = "criteo"
+    if "name" in dataset_config:
+        name = dataset_config.pop("name")
+
+    if name == "criteo":
+        dataset_cls = get_dataset_cls(dataloader_config)
+    elif name == "avazu_on_ram" or name == "avazu":
+        dataset_cls = AvazuOnRam
+
+    logger.info(f"Datset cls: {dataset_cls}")
+    return dataset_cls(**dataset_config, **train_info_to_val)
