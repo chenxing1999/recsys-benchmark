62583d1cb1c6ff631c13177832a31bff94169609
Hung V. Tran <h.v.tran@uq.edu.au>
2023-11-29 17:49:09 +1000

    Fix bug

--- src/models/embeddings/deepfm_opt_embed.py
+++ src/models/embeddings/deepfm_opt_embed.py
@@ -544,9 +544,11 @@ def evol_search_deepfm(
     with torch.no_grad():
         sub_mask = model.embedding.get_submask()
 
-    cur_ele_percent = sub_mask.sum() / model.embedding._num_item
+    d_target_sparsity = None
+    if target_sparsity is not None:
+        cur_ele_percent = sub_mask.sum() / model.embedding._num_item
+        d_target_sparsity = 1 - (1 - target_sparsity) / cur_ele_percent
 
-    d_target_sparsity = 1 - (1 - target_sparsity) / cur_ele_percent
     logger.debug(f"{d_target_sparsity=}")
     candidates = [
         _generate_candidate(
