5b5ff066e89a21ba0dabaac91dc24b45b21e1bff
Hung V. Tran <h.v.tran@uq.edu.au>
2024-03-30 15:28:22 +1000

    Refactor to warping all function in Trainer

---
+++ scripts/cf_train/train_cf.py
@@ -0,0 +1,139 @@
+import argparse
+import os
+from typing import Dict, Optional, Sequence
+
+import torch
+import yaml
+from torch.utils.data import DataLoader
+
+from src import metrics
+from src.dataset.cf_graph_dataset import CFGraphDataset, TestCFGraphDataset
+from src.loggers import Logger
+from src.trainer import get_cf_trainer
+from src.utils import set_seed
+
+set_seed(2023)
+
+
+def get_config(argv: Optional[Sequence[str]] = None) -> Dict:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config_file")
+    args = parser.parse_args(argv)
+    with open(args.config_file) as fin:
+        config = yaml.safe_load(fin)
+    return config
+
+
+def main(argv: Optional[Sequence[str]] = None):
+    config = get_config(argv)
+    logger = Logger(**config["logger"])
+
+    # Loading train dataset
+    logger.info("Load train dataset...")
+    train_dataloader_config = config["train_dataloader"]
+    train_dataset_config = train_dataloader_config["dataset"]
+    train_dataset = CFGraphDataset(**train_dataset_config)
+    logger.info("Successfully load train dataset")
+    train_dataset.describe()
+    train_dataloader = DataLoader(
+        train_dataset,
+        train_dataloader_config["batch_size"],
+        shuffle=True,
+        num_workers=train_dataloader_config["num_workers"],
+    )
+
+    if config["run_test"]:
+        val_dataloader_config = config["test_dataloader"]
+    else:
+        val_dataloader_config = config["val_dataloader"]
+
+    logger.info("Load val dataset...")
+    val_dataset = TestCFGraphDataset(val_dataloader_config["dataset"]["path"])
+    val_dataloader = DataLoader(
+        val_dataset,
+        val_dataloader_config["batch_size"],
+        shuffle=False,
+        collate_fn=TestCFGraphDataset.collate_fn,
+        num_workers=val_dataloader_config["num_workers"],
+    )
+    logger.info("Successfully load val dataset")
+
+    checkpoint_folder = os.path.dirname(config["checkpoint_path"])
+    os.makedirs(checkpoint_folder, exist_ok=True)
+
+    trainer = get_cf_trainer(
+        train_dataset.num_users,
+        train_dataset.num_items,
+        config,
+    )
+    model = trainer.model
+    model_config = config["model"]
+    logger.info(f"Trainer: {trainer}")
+
+    if config["run_test"]:
+        checkpoint = torch.load(config["checkpoint_path"])
+        model.load_state_dict(checkpoint["state_dict"], strict=False)
+
+        val_metrics = trainer.validate_epoch(
+            train_dataset, val_dataloader, metrics=["recall", "ndcg"]
+        )
+        for key, value in val_metrics.items():
+            logger.info(f"{key} - {value:.4f}")
+
+        return
+
+    num_params = 0
+    for p in model.parameters():
+        num_params += p.numel()
+    logger.log_metric("num_params", num_params)
+
+    logger.info(f"Model config: {model_config}")
+
+    best_ndcg = 0
+    num_epochs = config["num_epochs"]
+
+    early_stop_count = 0
+    early_stop_config = config.get("early_stop_patience", 0)
+    warmup = config.get("warmup", 0)
+    for epoch_idx in range(num_epochs):
+        logger.log_metric("Epoch", epoch_idx, epoch_idx)
+        train_metrics = trainer.train_epoch(train_dataloader)
+
+        train_metrics.update(metrics.get_env_metrics())
+        for metric, value in train_metrics.items():
+            logger.log_metric(f"train/{metric}", value, epoch_idx)
+
+        if (epoch_idx + 1) % config["validate_step"] == 0:
+            val_metrics = trainer.validate_epoch(
+                train_dataset,
+                val_dataloader,
+            )
+
+            val_metrics.update(metrics.get_env_metrics())
+
+            for key, value in val_metrics.items():
+                logger.log_metric(f"val/{key}", value, epoch_idx)
+
+            if best_ndcg < val_metrics["ndcg"]:
+                logger.info("New best, saving model...")
+                best_ndcg = val_metrics["ndcg"]
+
+                checkpoint = {
+                    "state_dict": model.state_dict(),
+                    "model_config": model_config,
+                    "val_metrics": val_metrics,
+                    "num_users": train_dataset.num_users,
+                    "num_items": train_dataset.num_items,
+                }
+                torch.save(checkpoint, config["checkpoint_path"])
+                early_stop_count = 0
+            elif warmup <= epoch_idx:
+                early_stop_count += 1
+                logger.debug(f"{early_stop_count=}")
+
+                if early_stop_config and early_stop_count > early_stop_config:
+                    return
+
+
+if __name__ == "__main__":
+    main()
